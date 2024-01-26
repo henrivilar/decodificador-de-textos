8df2580d3ad22796c4cc9e55078ce9389589c61b
henrivilar <fabio.fv1@hotmail.com>
2024-01-26 10:20:52 -0300

    algumas mudanças

--- script.js
+++
@@ -1,6 +0,0 @@
-function receberTexto(string) {
-    const inputElement = document.getElementById('inputTexto');
-    const texto = inputElement.value;
-    const textoRevertido = texto.split('').reverse().join('');
-    console.log(textoRevertido);
-}
